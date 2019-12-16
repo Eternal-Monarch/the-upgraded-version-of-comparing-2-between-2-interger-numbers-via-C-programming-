48c3d47ee2be285359e68b32af648395d868e790
SubhasishDutta-Coder <58630896+SubhasishDutta-Coder@users.noreply.github.com>
2019-12-17 05:59:56 +0600

    Add files via upload

---
+++ upgraded version of comparing between two integer numbers.c
@@ -0,0 +1,60 @@
+#include<stdio.h>
+int
+main ()
+{
+
+int a, b, sum;
+
+
+
+printf ("Enter the 1st number\n");
+
+
+
+scanf ("%d", &a);
+
+
+printf ("Enter the 2nd number\n");
+
+
+
+scanf ("%d", &b);
+
+
+
+if (a == b)
+
+    {
+
+printf ("Both are equal", a && b);
+
+}
+
+  {
+
+if (a < b)
+
+      {
+
+printf ("B is hiigher then A\n", b);
+
+
+}
+
+if (a > b)
+
+printf ("A is higher number then B\n", a);
+
+}
+
+
+
+return 0;
+
+
+}
+
+
+
+
+
